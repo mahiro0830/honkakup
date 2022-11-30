abb13784e9615f2caa46822010424628244a7d34
峯岸真弘 <shishisus4@gmail.com>
2022-12-01 03:08:55 +0900

    ファーストコミット

---
+++ html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+</head>
+<body>
+    <table border="1">
+        <tbody>
+            <tr>
+                <th><a href="section2/2-8.php">2-2-8</a></th>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
